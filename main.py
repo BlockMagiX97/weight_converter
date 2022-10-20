8bca7dd07c317c8c6b75a10302f7e4e7e4ded5d2
Maxmilian Mrazek <blockmagix97@gmail.com>
2022-10-20 18:48:32 +0000

    Added basic structure

--- main.py
+++ main.py
@@ -14,3 +14,12 @@ def update(list_label, units, weight):
         list_label[i].config(text=f"{weight_after} {units[i]}")
 
 
+root = tkinter.Tk()
+
+weightEntry = tkinter.Entry()
+weightEntry.insert(0, "Insert a weight in kgs")
+weightEntry.pack(side=tkinter.LEFT)
+
+updateButton = tkinter.Button(text="UPDATE", command=lambda: update('remove', ["grams", "lbs"], float(weightEntry.get())))
+
+root.mainloop()
